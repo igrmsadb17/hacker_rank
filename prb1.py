d06326774b5139cd9cd7709f50508ebf6ffbfd81
igrmsadb17 <balmikiavijitkv@gmail.com>
2026-05-30 15:17:29 +0530

    resolved solution

--- prb1.py
+++ prb1.py
@@ -1,5 +1,4 @@
-# in question no string methods should be used to get output
-# but i used str() to convert integers into str and join() to join these strings into one 
+
 def main(n):
     if n in range(1,151):
         for i in range(1, n+1):
