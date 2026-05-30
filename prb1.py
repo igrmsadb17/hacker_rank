95c5604915a0aee79f5a586f0d2f11f26075d906
igrmsadb17 <balmikiavijitkv@gmail.com>
2026-05-30 15:07:51 +0530

    right solution

--- prb1.py
+++ prb1.py
@@ -1,11 +1,10 @@
 def main(n):
     if n in range(1,151):
-        L = []
         for i in range(1, n+1):
-            L.append(str(i))
-        return "".join(L)
+            # end = "" will print the int in a single line without spaces
+            print(i, end = "")
+
     
 if __name__ == "__main__":
     n = int(input("n:"))
-    x = main(n)
-    print(x)
+    main(n)
